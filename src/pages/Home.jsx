fc031e13e5add3a82102e88ef06a4d71064e19b2
NikolajF <cph-nf70@cphbusiness.dk>
2024-12-08 15:19:38 +0100

    Added icon to website tab, and logo to Home.jsx. Should be fairly responsive.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,27 @@
+import styled from "styled-components";
+import TunesTableLogo from "../assets/TunesTable-Logo.png";
+
+const ImageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px;
+    width: 100%;
+`
+
+const StyledImage = styled.img`
+    transform: scale(0.9);
+    max-width: 100%;
+    height: auto;
+    border-radius: 8px;
+`
+
 function Home() {
     return (
         <>
+        <ImageContainer>
+            <StyledImage src={TunesTableLogo} alt="TunesTable Logo" />
+        </ImageContainer>
         <h1>TunesTable - music game/quiz api</h1>
         <p>Vores ide er at lave en musik API med brug af Spotify's web API for developers. Vores API kan holde kunstnere og albummer med tilhørende numre. Intentionen med vores API er det ville kunne blive brugt af os selv eller andre til at kreere en hjemmeside eller blot et program, hvilket vil kunne bruge information om de kunstnere, album og sange til at skabe en form for musik-quiz spil eller lignende. </p>     
         <br />
